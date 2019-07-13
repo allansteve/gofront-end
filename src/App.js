a89c02e8ede768160aa4a1f04f9892f9b2731882
allansteve <51958410+allansteve@users.noreply.github.com>
2019-07-13 17:36:30 -0300

    creat
    creat

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import profile from "./assets/Perfil.jpeg";
+
+function App() {
+  return <img width="200" src={profile} />;
+}
+
+export default App;
